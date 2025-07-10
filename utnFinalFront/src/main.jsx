addd96786e99a6a368850710d8868d0b1c8f49bb
josediegosantillan <santillanjosediego@gmail.com>
2025-07-10 16:58:16 -0300

    antes borre algo sin querer

--- utnFinalFront/src/main.jsx
+++ utnFinalFront/src/main.jsx
@@ -1,18 +1,16 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom'; // <--- ¡Importamos BrowserRouter!
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline'; // Para un reset de CSS básico
 import App from './App.jsx';
 import TemaPersonalizado from './temaPersonalizado'; // Importamos nuestro tema
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <ThemeProvider theme={TemaPersonalizado}>
       <CssBaseline /> {/* Opcional: Provee un CSS básico para normalizar estilos */}
       <BrowserRouter> 
          <App />
       </BrowserRouter>
     </ThemeProvider>
-  </React.StrictMode>
 );
